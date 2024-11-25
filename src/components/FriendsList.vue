8b67f7873d730ef20e3fa54997a7322cb9ae8735
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-11-25 16:04:18 +0100

    draggable

--- src/components/FriendsList.vue
+++ src/components/FriendsList.vue
@@ -1,41 +1,51 @@
 <template>
-  <q-list separator bordered padding>
-    <q-item
-      v-for="friend in Object.values(friends)"
-      :key="friend.id"
-      :style="{ borderLeft: `6px solid ${friend.color}` }"
-    >
-      <q-item-section avatar>
-        <q-avatar :color="friend.color" text-color="white">
-          {{ friend.name[0] }}</q-avatar
+  <draggable
+    v-model="friendsArray"
+    tag="div"
+    item-key="name"
+    v-bind="dragOptions"
+  >
+    <template #item="{ element }">
+      <q-list separator bordered padding>
+        <q-item
+          :key="element.id"
+          :style="{ borderLeft: `6px solid ${element.color}` }"
+          class="cursor-grab"
         >
-      </q-item-section>
-      <q-item-section>
-        <q-item-label>
-          {{ friend.name }}
-        </q-item-label>
-        <q-item-label caption>
-          {{ friend.email }}
-        </q-item-label>
-      </q-item-section>
-      <q-item-section side>
-        <div>
-          <q-chip
-            :label="isOSDetailsMap[friend.os].label"
-            :color="isOSDetailsMap[friend.os].color"
-            :icon="isOSDetailsMap[friend.os].icon"
-            class="text-white"
-          />
-          <q-icon :name="friend.is_human ? mdiHuman : mdiDog" size="sm">
-          </q-icon>
-        </div>
-      </q-item-section>
-    </q-item>
-  </q-list>
+          <q-item-section avatar>
+            <q-avatar :color="element.color" text-color="white">
+              {{ element.name[0] }}</q-avatar
+            >
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              {{ element.name }}
+            </q-item-label>
+            <q-item-label caption>
+              {{ element.email }}
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <div>
+              <q-chip
+                :label="isOSDetailsMap[element.os].label"
+                :color="isOSDetailsMap[element.os].color"
+                :icon="isOSDetailsMap[element.os].icon"
+                class="text-white"
+              />
+              <q-icon :name="element.is_human ? mdiHuman : mdiDog" size="sm">
+              </q-icon>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </template>
+  </draggable>
 </template>
 
 <script setup>
 import { friends } from 'assets/friends.js';
+import draggable from 'vuedraggable';
 import {
   mdiHuman,
   mdiDog,
@@ -43,12 +53,15 @@ import {
   mdiLinux,
   mdiApple,
 } from '@quasar/extras/mdi-v7';
+import { computed, ref } from 'vue';
 
 /* SI JE VEUX METTRE EN FORME UN WIDGET A PARTIR D'UNE SEULE DONNEE
 Je peux "map" un objet qui contiendra les différents proporiétés lié à cette donnée
 dans le template, je ferais référence à la propriété en fonction de la donnée
 ex: isOSDetailsMap[friend.os].icon
 */
+
+const friendsArray = ref(Object.values(friends));
 
 const isOSDetailsMap = {
   Windows: {
@@ -67,6 +80,15 @@ const isOSDetailsMap = {
     icon: mdiLinux,
   },
 };
+
+const dragOptions = computed(() => {
+  return {
+    animation: 200,
+    group: 'description',
+    disabled: false,
+    ghostClass: 'ghost',
+  };
+});
 </script>
 
 <style lang="scss" scoped></style>
