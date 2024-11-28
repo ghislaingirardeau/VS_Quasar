6cdf4c633c30b7a74e8a29501d530b1cb6ff7a35
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-11-28 10:27:42 +0100

    feat: cart total item

--- src/layouts/ShoppingLayout.vue
+++ src/layouts/ShoppingLayout.vue
@@ -1,7 +1,11 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-bar class="bg-blue-5">
+    <q-bar class="bg-blue-5 q-pa-lg">
       <h4>Shopping List</h4>
+      <q-space />
+      <q-btn size="md" flat :icon="mdiCart" class="q-ml-lg">
+        <q-badge rounded color="red" floating>{{ totalItems }}</q-badge>
+      </q-btn>
     </q-bar>
 
     <q-page-container>
@@ -10,6 +14,17 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { mdiCart } from '@quasar/extras/mdi-v7';
+import { useLocalStorage } from '@vueuse/core';
+import { Item } from 'src/types/index';
+import { computed, Ref } from 'vue';
+
+const shoppingItems: Ref<Item[]> = useLocalStorage('currentShopping', []);
+
+const totalItems = computed(() => {
+  return shoppingItems.value.length;
+});
+</script>
 
 <style scoped></style>
