51d366d5f41fa543750b04b0aa92307f7ca1b1c9
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-04 11:57:47 +0100

    refactor install prompt

--- src/components/shopping/DownloadWidget.vue
+++ src/components/shopping/DownloadWidget.vue
@@ -1,6 +1,6 @@
 <template>
   <q-btn
-    v-if="hasToInstall"
+    v-if="deferredPrompt"
     size="sm"
     round
     :icon="mdiDownload"
@@ -14,34 +14,17 @@
 import { mdiDownload } from '@quasar/extras/mdi-v7';
 import { Ref, ref } from 'vue';
 const deferredPrompt: Ref<any | null> = ref(null);
-const hasToInstall = ref(false);
 
-const handleInstallPrompt = (e: Event) => {
-  // Prevent the mini-infobar from appearing on mobile
+window.addEventListener('beforeinstallprompt', (e: Event) => {
   e.preventDefault();
-  // Stash the event so it can be triggered later.
   deferredPrompt.value = e;
-  // Update UI notify the user they can install the PWA
-  // Optionally, send analytics event that PWA install promo was shown.
   console.log("'beforeinstallprompt' event was fired.");
-  hasToInstall.value = true;
-};
-
-window.addEventListener('beforeinstallprompt', handleInstallPrompt);
+});
 
 async function handlePromptInstall() {
-  // Hide the app provided install promotion
-  // Show the install prompt
-  deferredPrompt.value.prompt();
-  // Wait for the user to respond to the prompt
-  const { outcome } = await deferredPrompt.value.userChoice;
-
-  console.log(outcome);
-
-  if (outcome !== 'dismissed') {
-    hasToInstall.value = false;
+  if (deferredPrompt.value) {
+    deferredPrompt.value.prompt();
   }
-  deferredPrompt.value = null;
 }
 </script>
 
