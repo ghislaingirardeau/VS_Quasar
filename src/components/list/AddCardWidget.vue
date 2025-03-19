568cdf8470e6844e5b4ac1c92d3bef72dae32589
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-03-19 16:38:16 +0700

    undo commit clean localstorage

---
+++ src/components/list/AddCardWidget.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-btn
+    size="sm"
+    round
+    :icon="mdiCreditCardPlusOutline"
+    class="q-ml-sm text-white"
+    @click="showDialogNewList"
+  >
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { mdiCreditCardPlusOutline } from '@quasar/extras/mdi-v7';
+import { useLists } from 'src/stores/lists';
+const listsStore = useLists();
+const { showDialogNewList } = listsStore;
+</script>
+
+<style scoped></style>
