0a39e243f21744abac3a62171f311f229fa556fe
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-03-24 16:08:07 +0700

    fix: tailwind dynamic classes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,4 +19,6 @@ export const colors = [
   'pink',
   'purple',
   'slate',
+  'teal',
+  'orange',
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // Tailwind purge par défaut les classes non présentes en dur dans ton code, donc si j'ajoute des classes dynamiquement, il faut les ajouter ici
+  // je dis donc à Tailwind que ces classes seront utilisées dynamiquement dans mon code
+  safelist: [
+    'bg-red-200',
+    'bg-blue-200',
+    'bg-green-200',
+    'bg-yellow-200',
+    'bg-purple-200',
+    'bg-pink-200',
+    'bg-indigo-200',
+    'bg-slate-200',
+    'bg-orange-200',
+    'bg-teal-200',
+  ],
   theme: {
     extend: {},
   },
