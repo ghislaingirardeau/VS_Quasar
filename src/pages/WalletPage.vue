0ee5bad3dfceb0e72f88c8d93b3d7bdea94e02e2
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-03-25 11:25:38 +0700

    barcode new test

--- src/pages/WalletPage.vue
+++ src/pages/WalletPage.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page :key="'wallet' + cards.length" padding>
-    <pre>{{ barCodeValue }}</pre>
+    <pre>{{ barcodeDetected }}</pre>
     <pre>{{ codeBarMessage }}</pre>
+    <img
+      id="barcode-image"
+      src="/src/assets/codeBarTest.jpg"
+      alt="Code-barres"
+    />
     <draggable v-model="cards" tag="div" item-key="id" v-bind="dragOptions">
       <template #item="{ element }">
         <q-card
@@ -66,34 +71,38 @@ const BarcodeDetector = window.BarcodeDetector || class {};
 // const barcodeDetector = new BarcodeDetector({
 //   formats: ['code_39', 'codabar', 'ean_13', 'code_128'],
 // });
-const imageEl = document.createElement('img');
-imageEl.src = 'src/assets/codeBarTest.jpg';
-const barCodeValue = ref(null);
-const codeBarMessage = ref('');
+
+const barcodeDetected = ref(null);
+const codeBarMessage = ref<string[]>([]);
 
 onMounted(() => {
-  console.log(imageEl);
-  if (!('BarcodeDetector' in globalThis)) {
-    codeBarMessage.value = 'Barcode Detector is not supported by this browser.';
-  } else {
-    codeBarMessage.value = 'Barcode Detector supported!';
-
-    // create new detector
-    const barcodeDetector = new BarcodeDetector({
-      formats: ['code_39', 'codabar', 'ean_13', 'code_128'],
-    });
-    barcodeDetector
-      .detect(imageEl)
-      .then((barcodes: { rawValue: string }[]) => {
-        barcodes.forEach(
-          (barcode: any) => (barCodeValue.value = barcode.rawValue),
-        );
-      })
-      .catch((err: any) => {
-        barCodeValue.value = err;
-      });
-  }
+  const imageEl = document.createElement('img');
+  imageEl.src = 'src/assets/codeBarTest.jpg';
+  if (imageEl) detectBarcode(imageEl);
 });
+
+const detectBarcode = async (imageElement: HTMLImageElement) => {
+  if (!('BarcodeDetector' in window) || !window.BarcodeDetector) {
+    console.error('BarcodeDetector API non supportée.');
+    return;
+  }
+  codeBarMessage.value.push('Détection en cours...');
+  try {
+    const barcodeDetector = new BarcodeDetector({
+      formats: ['qr_code', 'code_128', 'ean_13'],
+    });
+    const barcodes = await barcodeDetector.detect(imageElement);
+    if (barcodes.length > 0) {
+      barcodeDetected.value = barcodes[0].rawValue;
+      codeBarMessage.value.push('Code-barres détecté ');
+
+      console.log('Code-barres détecté :', barcodeDetected.value);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la détection :', error);
+    codeBarMessage.value.push('Ereur lors de la détection');
+  }
+};
 
 const selectedCard = ref<Ref<Card> | null>(null);
 
